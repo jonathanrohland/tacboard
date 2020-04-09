d82212fa6c7ecefddb3ef509497e542a8b4f17cd
Jonathan Rohland <jonathan.rohland@tngtech.com>
2020-04-10 00:32:05 +0200

    Show number of marbles in house

--- src/components/House.tsx
+++ src/components/House.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 import { connect } from 'react-redux';
 import classnames from 'classnames';
 
+import { ActionType } from '../actions';
 import { Color, State } from '../types';
 import './House.css';
-import { ActionType } from '../actions';
 
 
 type OwnProps = {
@@ -12,7 +12,8 @@ type OwnProps = {
 }
 
 type StateProps = {
-    isPickedUp: boolean
+    numMarbles: number,
+    isPickedUp: boolean,
 }
 
 type DispatchProps = {
@@ -22,12 +23,23 @@ type DispatchProps = {
 type Props = OwnProps & StateProps & DispatchProps;
 
 
-function Field(props: Props) {
-    return <div onClick={props.onHouseClick} className={classnames("house", `house--${props.color.toString()}`, props.isPickedUp ? 'house--picked-up' : null)} />
+function House(props: Props) {
+    return (
+        <div onClick={props.onHouseClick} className={classnames("house", `house--${props.color.toString()}`, props.isPickedUp ? 'house--picked-up' : null)}>
+            <div className="house__marble-container">
+                {Array.from(Array(props.numMarbles).keys()).map(index => <div className={classnames("house__marble")} />)}
+            </div>
+        </div>
+    );
+}
+
+function countMarblesOfColor(state: State, color: Color) {
+    return Object.values(state.marblePositions!).reduce((count, marbleColor) => marbleColor === color ? count + 1 : count, 0);
 }
 
 function mapStateToProps(state: State, { color }: OwnProps): StateProps {
     return {
+        numMarbles: 4 - countMarblesOfColor(state, color),
         isPickedUp: state.pickedUpMarble?.field === null && state.pickedUpMarble?.color === color
     }
 }
@@ -38,4 +50,4 @@ const mapDispatchToProps = (dispatch: any, { color }: OwnProps): DispatchProps =
     }
 }
 
-export default connect<StateProps, DispatchProps, OwnProps>(mapStateToProps, mapDispatchToProps)(Field);
+export default connect<StateProps, DispatchProps, OwnProps>(mapStateToProps, mapDispatchToProps)(House);
