5a0b23279a85e1b866a1a19f5c99f86f7a7c8c7e
Jonathan Rohland <jonathan.rohland@tngtech.com>
2020-04-14 08:08:31 +0200

    Swap light and dark background on fields

--- src/Websocket.ts
+++ src/Websocket.ts
@@ -1,11 +1,18 @@
 let webSocket: WebSocket;
 
+function initialiseConnection(socket: WebSocket) {
+    socket.send(JSON.stringify({
+        'message': 'initSession',
+    }))
+}
+
 
 function openWebsocket() {
     webSocket = new WebSocket("wss://qdgv7zrkrl.execute-api.eu-central-1.amazonaws.com/Prod");
 
     webSocket.onopen = () => {
         console.log('Opened Websocket connection.');
+        initialiseConnection(webSocket);
     }
 
     return webSocket;
