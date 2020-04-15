1e974a3b91fc2c766a45ebde53e5d17e3fbf4a9b
Jonathan Rohland <jonathan.rohland@tngtech.com>
2020-04-15 20:19:10 +0200

    Update websocket-url

--- src/Websocket.ts
+++ src/Websocket.ts
@@ -1,5 +1,7 @@
 let webSocket: WebSocket;
 
+const WEBSOCKET_URL = "wss://sd8oo3pxl1.execute-api.eu-central-1.amazonaws.com/Prod";
+
 function initialiseConnection(socket: WebSocket, gameId: string) {
     socket.send(JSON.stringify({
         'message': 'initSession',
@@ -9,7 +11,7 @@ function initialiseConnection(socket: WebSocket, gameId: string) {
 
 
 function openWebsocket() {
-    webSocket = new WebSocket("wss://rjpbuvbbpc.execute-api.eu-central-1.amazonaws.com/Prod");
+    webSocket = new WebSocket(WEBSOCKET_URL);
 
     webSocket.onopen = () => {
         console.log('Opened Websocket connection.');
