c3344405d4f040f129ab5ced9ed1cbf4fbf08143
Jonathan Rohland <jonathan.rohland@tngtech.com>
2020-04-18 18:18:14 +0200

    Add link to board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -6,18 +6,17 @@ import Field from './Field';
 import './Board.css';
 import { FieldIndex, Color } from '../types';
 import House from './House';
+import { useTranslation } from 'react-i18next';
 
 
-function Board(props: {
 
-}) {
-    console.log('Board props:', props);
+function Board() {
+    const { t } = useTranslation();
     const fieldIndexes = Array.from(Array(80).keys()) as Array<FieldIndex>;
-    console.log('indexes:', fieldIndexes);
-
 
     return (
         <div className={classnames("Board")}>
+            <div>{`${t('board__game-link-prefix')}: ${document.location}`}</div>
             {fieldIndexes.map(fieldIndex => <Field index={fieldIndex} key={fieldIndex} />)}
             {[Color.RED, Color.BLUE, Color.GREEN, Color.BLACK].map(color => <House color={color} key={color} />)}
         </div>
--- src/i18n.ts
+++ src/i18n.ts
@@ -12,6 +12,7 @@ export function initI18n() {
                         "landing-page__explanation": "Bitte wählen Sie ein Spiel-Kennwort und schicken Sie es an ihre Mitspieler. Das Kennwort darf nur Buchstaben und Zahlen enthalten.",
                         "landing-page__start-game": "Spiel Starten",
                         "landing-page__input-error": "Das Kennwort darf nur Buchstaben und Zahlen enthalten. Bitte wählen Sie ein anderes Passwort!",
+                        "board__game-link-prefix": "Schicke deinen Mitspielern diesen Link",
                     }
                 }
             },
