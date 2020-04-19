60d73873e548627de54caa701f5054414d85b82a
Jonathan Rohland <jonathan.rohland@tngtech.com>
2020-04-19 12:07:31 +0200

    Open external links in new tab

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -13,11 +13,11 @@ function Footer() {
             <div className="footer__content">
                 <a href="/about.html" className="footer__link">{t('footer__about-link')}</a>
                 <a href="/privacy.html" className="footer__link">{t('footer__privacy-link')}</a>
-                <a className="footer__link" href="https://github.com/jonathanrohland/tacboard-frontend">
+                <a className="footer__link" target="_blank" href="https://github.com/jonathanrohland/tacboard-frontend">
                     <img className="footer__github-icon" src={githubIcon} alt={t('github-icon-alt-text')} />
                     Sieh dir den Code an
                 </a>
-                <a href="https://www.buymeacoffee.com/tacbrett" className="footer__link">{t('footer__buy-me-a-beer-link')}</a>
+                <a href="https://www.buymeacoffee.com/tacbrett" target="_blank" className="footer__link">{t('footer__buy-me-a-beer-link')}</a>
             </div>
         </div>
     );
