f2b796e526d8e483f1925ebd602075c036e54055
Jonathan Rohland <jonathan.rohland@tngtech.com>
2020-04-19 13:52:41 +0200

    Add footer to Board

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -7,6 +7,7 @@ import './Board.css';
 import { FieldIndex, Color } from '../types';
 import House from './House';
 import { useTranslation } from 'react-i18next';
+import Footer from './Footer';
 
 
 
@@ -15,10 +16,14 @@ function Board() {
     const fieldIndexes = Array.from(Array(80).keys()) as Array<FieldIndex>;
 
     return (
-        <div className={classnames("Board")}>
-            <div>{t('board__game-link-prefix')}: <a href={document.location.toString()}>{document.location.toString()}</a></div>
-            {fieldIndexes.map(fieldIndex => <Field index={fieldIndex} key={fieldIndex} />)}
-            {[Color.RED, Color.BLUE, Color.GREEN, Color.BLACK].map(color => <House color={color} key={color} />)}
+        <div>
+
+            <div className={classnames("Board")}>
+                <div>{t('board__game-link-prefix')}: <a href={document.location.toString()}>{document.location.toString()}</a></div>
+                {fieldIndexes.map(fieldIndex => <Field index={fieldIndex} key={fieldIndex} />)}
+                {[Color.RED, Color.BLUE, Color.GREEN, Color.BLACK].map(color => <House color={color} key={color} />)}
+            </div>
+            <Footer />
         </div>
     );
 }
